50abf9d0901924dee13b7afbf9c16049f313b59b
Kevin <kevinbuangjog@gmail.com>
2025-09-14 13:40:33 +0800

    Update game assets and styles; add new images and change font to 'Press Start 2P'

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -10,11 +10,20 @@ export class MainMenu extends Scene
     create ()
     {
         this.add.image(512, 384, 'background');
+        
+        this.add.text(512, 280, 'PATINTERO', {
+            fontFamily: '"Press Start 2P"', fontSize: 100, color: '#f3a51d',
+            stroke: '#000000', strokeThickness: 10,
+            align: 'center'
+        }).setOrigin(0.5)
+        this.add.text(512, 370, 'LEGACY', {
+            fontFamily: '"Press Start 2P"', fontSize: 80, color: '#1977bd',
+            stroke: '#000000', strokeThickness: 10,
+            align: 'center'
+        }).setOrigin(0.5)
 
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        this.add.text(512, 600, 'Main Menu', {
+            fontFamily: '"Press Start 2P"', fontSize: 30, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
