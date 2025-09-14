6cb9573ea345c0bb4772d5f42351b595a0585b5a
Kevin <kevinbuangjog@gmail.com>
2025-09-15 00:35:32 +0800

    Refractor animation logic

--- src/game/scenes/TestPlayGrounds.js
+++ src/game/scenes/TestPlayGrounds.js
@@ -45,32 +45,32 @@ export class TestPlayGrounds extends Scene
         // });
 
         const anims = [
-            { key: 'turn', start: 0, end: 0, frameRate: 20, repeat: 0 },
-            { key: 'right', start: 4, end: 7, frameRate: 8, repeat: -1 },
-            { key: 'left', start: 8, end: 11, frameRate: 8, repeat: -1 },
-            { key: 'up', start: 12, end: 15, frameRate: 8, repeat: -1 },
-            { key: 'down', start: 16, end: 19, frameRate: 8, repeat: -1 }
-        ];
+        { key: 'turn', start: 0, end: 0, frameRate: 20, repeat: 0 },
+        { key: 'right', start: 4, end: 7, frameRate: 8, repeat: -1 },
+        { key: 'left', start: 8, end: 11, frameRate: 8, repeat: -1 },
+        { key: 'up', start: 12, end: 15, frameRate: 8, repeat: -1 },
+        { key: 'down', start: 16, end: 19, frameRate: 8, repeat: -1 }
+    ];
 
-        anims.forEach(anim => {
-            // For the 'turn' animation, use a single frame.
-            if (anim.key === 'turn') {
-                this.anims.create({
-                    key: anim.key,
-                    frames: [{ key: 'player', frame: anim.start }],
-                    frameRate: anim.frameRate,
-                    repeat: anim.repeat
-                });
-            } else {
-                // For all other animations, generate the frame range.
-                this.anims.create({
-                    key: anim.key,
-                    frames: this.anims.generateFrameNumbers('player', { start: anim.start, end: anim.end }),
-                    frameRate: anim.frameRate,
-                    repeat: anim.repeat
-                });
-            }
-        });
+    anims.forEach(anim => {
+        // For the 'turn' animation, use a single frame.
+        if (anim.key === 'turn') {
+            this.anims.create({
+                key: anim.key,
+                frames: [{ key: 'player', frame: anim.start }],
+                frameRate: anim.frameRate,
+                repeat: anim.repeat
+            });
+        } else {
+            // For all other animations, generate the frame range.
+            this.anims.create({
+                key: anim.key,
+                frames: this.anims.generateFrameNumbers('player', { start: anim.start, end: anim.end }),
+                frameRate: anim.frameRate,
+                repeat: anim.repeat
+            });
+        }
+    });
     }
 
     create()
