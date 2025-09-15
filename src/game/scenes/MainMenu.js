8b16f8788dbbf66168e903881ae7cf0772ae72ef
Kevin <kevinbuangjog@gmail.com>
2025-09-15 11:37:47 +0800

    Added floating text effect on Logo

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -10,17 +10,34 @@ export class MainMenu extends Scene
     create ()
     {
         this.add.image(512, 384, 'background').setScale(1.5);
-        
-        this.add.text(512, 280, 'PATINTERO', {
+        // Game Title
+        this.title = this.add.text(512, 280, 'PATINTERO', {
             fontFamily: '"Press Start 2P"', fontSize: 100, color: '#f3a51d',
             stroke: '#000000', strokeThickness: 10,
             align: 'center'
         }).setOrigin(0.5)
-        this.add.text(512, 370, 'LEGACY', {
+        this.titleSub = this.add.text(512, 370, 'LEGACY', {
             fontFamily: '"Press Start 2P"', fontSize: 80, color: '#1977bd',
             stroke: '#000000', strokeThickness: 10,
             align: 'center'
         }).setOrigin(0.5)
+
+        this.tweens.add({
+            targets: this.title,
+            y: this.title.y - 20, // Move 20 pixels up from its original position
+            duration: 1500, // Duration of the tween in milliseconds
+            ease: 'Sine.easeInOut',
+            yoyo: true, // Make it go back and forth
+            repeat: -1 // Repeat forever
+        });
+        this.tweens.add({
+            targets: this.titleSub,
+            y: this.titleSub.y - 20, // Move 20 pixels down from its original position
+            duration: 1500,
+            ease: 'Sine.easeInOut',
+            yoyo: true,
+            repeat: -1
+        });
 
         // Clickable Play Text
         this.mainMenuText = this.add.text(512, 600, 'Play', {
